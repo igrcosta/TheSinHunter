1caada280577f93fdd9612802e6b77639593892f
Igor Costa <igorcosta250705@gmail.com>
2026-03-10 15:57:33 -0300

    Testes no parry V2
    É notável que as posições do parry vão precisar ser pensadas baseadas na velocidade atual do jogador, provavelmente, terá que haver chunks separadas na vel média do player

--- Assets/Scripts/ParryScript.cs
+++ Assets/Scripts/ParryScript.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 
 public class ParryScript : MonoBehaviour
 {
-    public Vector3 ParryEffect = new Vector3 (0f, 9.81f + 9.81f/2, 0f);
+    public Vector3 ParryEffect = new Vector3 (0f, 9.81f*3, 0f);
     void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
